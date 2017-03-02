dc89b76e8704c718d4de5cdc7b633b143f3d7bf6
Martin Rancourt <mrancourt@users.noreply.github.com>
2017-03-02 03:24:34 -0500

    enabled chome devtools extension for redux (#22)

--- client/containers/Root.dev.js
+++ client/containers/Root.dev.js
@@ -11,7 +11,6 @@ export default class Root extends Component {
       <Provider store={store}>
           <div>
               <Router history={history} routes={routes} />
-              <DevTools />
           </div>
       </Provider>
     );
