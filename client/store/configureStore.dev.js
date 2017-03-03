d29de414f9461b5ddccede30bf63901d93df92eb
Martin Rancourt <vonziper@gmail.com>
2017-03-03 11:53:08 -0500

    redux devtools fix

--- client/store/configureStore.dev.js
+++ client/store/configureStore.dev.js
@@ -3,12 +3,9 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(rootReducer, initialState, composeEnhancers(
+    applyMiddleware(thunk)
     )
   );
 
