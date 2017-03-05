5956575fa0464ed737ac94d0a850e9087fb5a559
Martin Rancourt <mrancourt@users.noreply.github.com>
2017-03-05 18:53:22 -0500

    added parameters editor (#25)

---
+++ client/components/EditorErrors.js
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react';
+import SyntaxError from './SyntaxError';
+
+class EditorErrors extends React.Component {
+  render() {
+    let { errors } = this.props;
+    return (
+      <div>
+        {errors.length > 0 ? (
+          <pre>
+            Errors in parameters. Please correct them before saving a template or executing a Backtest.
+            <code style={{color: 'red', margin: 0}}>
+            { errors.map((error, index) => {
+              return <SyntaxError key={index} error={error} />;
+            }) }
+            </code>
+          </pre>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+
+SyntaxError.propTypes = {
+  errors: PropTypes.object
+};
+
+export default EditorErrors;
