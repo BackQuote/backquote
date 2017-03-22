7af4da55e087e43a66ef52375e0c30a1060327a4
Anthony <a.nadeau97@hotmail.ca>
2017-03-21 20:36:20 -0400

    updated sb schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,41 +5,43 @@
 --
 -- Cleaning up tables and sequences
 --
-DROP TABLE IF EXISTS algorithms CASCADE;
+DROP TABLE IF EXISTS algorithm CASCADE;
 DROP TABLE IF EXISTS backtest CASCADE;
 DROP TABLE IF EXISTS backtest_ticker CASCADE;
 DROP TABLE IF EXISTS day CASCADE;
-DROP TABLE IF EXISTS quotes CASCADE;
+DROP TABLE IF EXISTS quote CASCADE;
 DROP TABLE IF EXISTS result CASCADE;
 DROP TABLE IF EXISTS simulation CASCADE;
 DROP TABLE IF EXISTS ticker CASCADE;
 DROP TABLE IF EXISTS trade CASCADE;
+DROP TABLE IF EXISTS template CASCADE;
 
-DROP SEQUENCE IF EXISTS algorithms_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS algorithm_id_seq CASCADE;
 DROP SEQUENCE IF EXISTS backtest_id_seq CASCADE;
 DROP SEQUENCE IF EXISTS day_id_seq CASCADE;
-DROP SEQUENCE IF EXISTS quotes_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS quote_id_seq CASCADE;
 DROP SEQUENCE IF EXISTS result_result_id_seq CASCADE;
 DROP SEQUENCE IF EXISTS simulation_id_seq CASCADE;
 DROP SEQUENCE IF EXISTS trade_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS template_id_seq CASCADE;
 
 --
--- Name: algorithm; Type: TABLE; Schema: public; Owner: backquote_user
+-- Name: algorithm; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE algorithm (
     id bigint NOT NULL,
-    name character varying(60)
+    name VARCHAR(60)
 );
 
 
-ALTER TABLE algorithm OWNER TO backquote_user;
+ALTER TABLE algorithm OWNER TO postgres;
 
 --
--- Name: algorithms_id_seq; Type: SEQUENCE; Schema: public; Owner: backquote_user
+-- Name: algorithm_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE algorithms_id_seq
+CREATE SEQUENCE algorithm_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -47,31 +49,32 @@ CREATE SEQUENCE algorithms_id_seq
     CACHE 1;
 
 
-ALTER TABLE algorithms_id_seq OWNER TO backquote_user;
+ALTER TABLE algorithm_id_seq OWNER TO postgres;
 
 --
--- Name: algorithms_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: backquote_user
+-- Name: algorithm_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE algorithms_id_seq OWNED BY algorithm.id;
+ALTER SEQUENCE algorithm_id_seq OWNED BY algorithm.id;
 
 
 --
--- Name: backtest; Type: TABLE; Schema: public; Owner: backquote_user
+-- Name: backtest; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE backtest (
     id integer NOT NULL,
-    "timestamp" timestamp without time zone,
+    timestamp timestamp without time zone,
     params text,
+    success boolean DEFAULT false NOT NULL,
     algorithm_id integer
 );
 
 
-ALTER TABLE backtest OWNER TO backquote_user;
+ALTER TABLE backtest OWNER TO postgres;
 
 --
--- Name: backtest_id_seq; Type: SEQUENCE; Schema: public; Owner: backquote_user
+-- Name: backtest_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE backtest_id_seq
@@ -82,17 +85,17 @@ CREATE SEQUENCE backtest_id_seq
     CACHE 1;
 
 
-ALTER TABLE backtest_id_seq OWNER TO backquote_user;
+ALTER TABLE backtest_id_seq OWNER TO postgres;
 
 --
--- Name: backtest_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: backquote_user
+-- Name: backtest_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE backtest_id_seq OWNED BY backtest.id;
 
 
 --
--- Name: backtest_ticker; Type: TABLE; Schema: public; Owner: backquote_user
+-- Name: backtest_ticker; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE backtest_ticker (
@@ -101,10 +104,10 @@ CREATE TABLE backtest_ticker (
 );
 
 
-ALTER TABLE backtest_ticker OWNER TO backquote_user;
+ALTER TABLE backtest_ticker OWNER TO postgres;
 
 --
--- Name: day; Type: TABLE; Schema: public; Owner: backquote_user
+-- Name: day; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE day (
@@ -113,10 +116,10 @@ CREATE TABLE day (
 );
 
 
-ALTER TABLE day OWNER TO backquote_user;
+ALTER TABLE day OWNER TO postgres;
 
 --
--- Name: day_id_seq; Type: SEQUENCE; Schema: public; Owner: backquote_user
+-- Name: day_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE day_id_seq
@@ -127,36 +130,39 @@ CREATE SEQUENCE day_id_seq
     CACHE 1;
 
 
-ALTER TABLE day_id_seq OWNER TO backquote_user;
+ALTER TABLE day_id_seq OWNER TO postgres;
 
 --
--- Name: day_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: backquote_user
+-- Name: day_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE day_id_seq OWNED BY day.id;
 
 
 --
--- Name: quote; Type: TABLE; Schema: public; Owner: backquote_user
+-- Name: quote; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE quote (
     id integer NOT NULL,
-    price numeric(7,2) NOT NULL,
-    "timestamp" timestamp without time zone,
+    open NUMERIC(7,2) NOT NULL,
+    high NUMERIC(7,2) NOT NULL,
+    low NUMERIC(7,2) NOT NULL,
+    close NUMERIC(7,2) NOT NULL,
+    timestamp TIMESTAMP,
     last_of_day boolean DEFAULT false NOT NULL,
     day_id integer,
-    ticker character varying(10)
+    ticker VARCHAR(10)
 );
 
 
-ALTER TABLE quote OWNER TO backquote_user;
+ALTER TABLE quote OWNER TO postgres;
 
 --
--- Name: quotes_id_seq; Type: SEQUENCE; Schema: public; Owner: backquote_user
+-- Name: quote_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE quotes_id_seq
+CREATE SEQUENCE quote_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -164,17 +170,17 @@ CREATE SEQUENCE quotes_id_seq
     CACHE 1;
 
 
-ALTER TABLE quotes_id_seq OWNER TO backquote_user;
+ALTER TABLE quote_id_seq OWNER TO postgres;
 
 --
--- Name: quotes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: backquote_user
+-- Name: quote_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE quotes_id_seq OWNED BY quote.id;
+ALTER SEQUENCE quote_id_seq OWNED BY quote.id;
 
 
 --
--- Name: result; Type: TABLE; Schema: public; Owner: backquote_user
+-- Name: result; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE result (
@@ -188,10 +194,10 @@ CREATE TABLE result (
 );
 
 
-ALTER TABLE result OWNER TO backquote_user;
+ALTER TABLE result OWNER TO postgres;
 
 --
--- Name: result_result_id_seq; Type: SEQUENCE; Schema: public; Owner: backquote_user
+-- Name: result_result_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE result_result_id_seq
@@ -202,33 +208,33 @@ CREATE SEQUENCE result_result_id_seq
     CACHE 1;
 
 
-ALTER TABLE result_result_id_seq OWNER TO backquote_user;
+ALTER TABLE result_result_id_seq OWNER TO postgres;
 
 --
--- Name: result_result_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: backquote_user
+-- Name: result_result_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE result_result_id_seq OWNED BY result.id;
 
 
 --
--- Name: simulation; Type: TABLE; Schema: public; Owner: backquote_user
+-- Name: simulation; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE simulation (
     id integer NOT NULL,
     backtest_id integer,
-    ticker character varying(10),
+    ticker VARCHAR(10),
     params text,
     profit_reset numeric(10,2),
     profit_no_reset numeric(10,2)
 );
 
 
-ALTER TABLE simulation OWNER TO backquote_user;
+ALTER TABLE simulation OWNER TO postgres;
 
 --
--- Name: simulation_id_seq; Type: SEQUENCE; Schema: public; Owner: backquote_user
+-- Name: simulation_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE simulation_id_seq
@@ -239,17 +245,17 @@ CREATE SEQUENCE simulation_id_seq
     CACHE 1;
 
 
-ALTER TABLE simulation_id_seq OWNER TO backquote_user;
+ALTER TABLE simulation_id_seq OWNER TO postgres;
 
 --
--- Name: simulation_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: backquote_user
+-- Name: simulation_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE simulation_id_seq OWNED BY simulation.id;
 
 
 --
--- Name: template; Type: TABLE; Schema: public; Owner: backquote_user
+-- Name: template; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE template (
@@ -259,13 +265,13 @@ CREATE TABLE template (
 );
 
 
-ALTER TABLE template OWNER TO backquote_user;
+ALTER TABLE template OWNER TO postgres;
 
 --
--- Name: templates_id_seq; Type: SEQUENCE; Schema: public; Owner: backquote_user
+-- Name: template_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE templates_id_seq
+CREATE SEQUENCE template_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -273,28 +279,28 @@ CREATE SEQUENCE templates_id_seq
     CACHE 1;
 
 
-ALTER TABLE templates_id_seq OWNER TO backquote_user;
+ALTER TABLE template_id_seq OWNER TO postgres;
 
 --
--- Name: templates_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: backquote_user
+-- Name: template_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE templates_id_seq OWNED BY template.id;
+ALTER SEQUENCE template_id_seq OWNED BY template.id;
 
 
 --
--- Name: ticker; Type: TABLE; Schema: public; Owner: backquote_user
+-- Name: ticker; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE ticker (
-    ticker character varying(10) NOT NULL
+    ticker VARCHAR(10) NOT NULL
 );
 
 
-ALTER TABLE ticker OWNER TO backquote_user;
+ALTER TABLE ticker OWNER TO postgres;
 
 --
--- Name: trade; Type: TABLE; Schema: public; Owner: backquote_user
+-- Name: trade; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE trade (
@@ -302,15 +308,15 @@ CREATE TABLE trade (
     quantity_reset integer,
     quantity_no_reset integer,
     action integer,
-    "timestamp" timestamp without time zone,
+    timestamp timestamp without time zone,
     result_id integer
 );
 
 
-ALTER TABLE trade OWNER TO backquote_user;
+ALTER TABLE trade OWNER TO postgres;
 
 --
--- Name: trade_id_seq; Type: SEQUENCE; Schema: public; Owner: backquote_user
+-- Name: trade_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE trade_id_seq
@@ -321,80 +327,80 @@ CREATE SEQUENCE trade_id_seq
     CACHE 1;
 
 
-ALTER TABLE trade_id_seq OWNER TO backquote_user;
+ALTER TABLE trade_id_seq OWNER TO postgres;
 
 --
--- Name: trade_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: backquote_user
+-- Name: trade_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE trade_id_seq OWNED BY trade.id;
 
 
 --
--- Name: algorithm id; Type: DEFAULT; Schema: public; Owner: backquote_user
+-- Name: algorithm id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY algorithm ALTER COLUMN id SET DEFAULT nextval('algorithms_id_seq'::regclass);
+ALTER TABLE ONLY algorithm ALTER COLUMN id SET DEFAULT nextval('algorithm_id_seq'::regclass);
 
 
 --
--- Name: backtest id; Type: DEFAULT; Schema: public; Owner: backquote_user
+-- Name: backtest id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY backtest ALTER COLUMN id SET DEFAULT nextval('backtest_id_seq'::regclass);
 
 
 --
--- Name: day id; Type: DEFAULT; Schema: public; Owner: backquote_user
+-- Name: day id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY day ALTER COLUMN id SET DEFAULT nextval('day_id_seq'::regclass);
 
 
 --
--- Name: quote id; Type: DEFAULT; Schema: public; Owner: backquote_user
+-- Name: quote id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY quote ALTER COLUMN id SET DEFAULT nextval('quotes_id_seq'::regclass);
+ALTER TABLE ONLY quote ALTER COLUMN id SET DEFAULT nextval('quote_id_seq'::regclass);
 
 
 --
--- Name: result id; Type: DEFAULT; Schema: public; Owner: backquote_user
+-- Name: result id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY result ALTER COLUMN id SET DEFAULT nextval('result_result_id_seq'::regclass);
 
 
 --
--- Name: simulation id; Type: DEFAULT; Schema: public; Owner: backquote_user
+-- Name: simulation id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY simulation ALTER COLUMN id SET DEFAULT nextval('simulation_id_seq'::regclass);
 
 
 --
--- Name: template id; Type: DEFAULT; Schema: public; Owner: backquote_user
+-- Name: template id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY template ALTER COLUMN id SET DEFAULT nextval('templates_id_seq'::regclass);
+ALTER TABLE ONLY template ALTER COLUMN id SET DEFAULT nextval('template_id_seq'::regclass);
 
 
 --
--- Name: trade id; Type: DEFAULT; Schema: public; Owner: backquote_user
+-- Name: trade id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY trade ALTER COLUMN id SET DEFAULT nextval('trade_id_seq'::regclass);
 
 
 --
--- Name: trade_id_seq; Type: SEQUENCE SET; Schema: public; Owner: backquote_user
+-- Name: trade_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
 SELECT pg_catalog.setval('trade_id_seq', 1, false);
 
 
 --
--- Name: algorithm algorithms_pkey; Type: CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: algorithm algorithms_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY algorithm
@@ -402,7 +408,7 @@ ALTER TABLE ONLY algorithm
 
 
 --
--- Name: backtest backtest_pkey; Type: CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: backtest backtest_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY backtest
@@ -410,7 +416,7 @@ ALTER TABLE ONLY backtest
 
 
 --
--- Name: day day_pkey; Type: CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: day day_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY day
@@ -418,7 +424,7 @@ ALTER TABLE ONLY day
 
 
 --
--- Name: quote quotes_pkey; Type: CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: quote quotes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY quote
@@ -426,7 +432,7 @@ ALTER TABLE ONLY quote
 
 
 --
--- Name: result result_pkey; Type: CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: result result_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY result
@@ -434,7 +440,7 @@ ALTER TABLE ONLY result
 
 
 --
--- Name: simulation simulation_pkey; Type: CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: simulation simulation_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY simulation
@@ -442,7 +448,7 @@ ALTER TABLE ONLY simulation
 
 
 --
--- Name: template templates_pkey; Type: CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: template templates_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY template
@@ -450,7 +456,7 @@ ALTER TABLE ONLY template
 
 
 --
--- Name: ticker ticker_ticker_pk; Type: CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: ticker ticker_ticker_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY ticker
@@ -458,7 +464,7 @@ ALTER TABLE ONLY ticker
 
 
 --
--- Name: trade trade_pkey; Type: CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: trade trade_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY trade
@@ -466,7 +472,7 @@ ALTER TABLE ONLY trade
 
 
 --
--- Name: backtest backtest_algorithm_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: backtest backtest_algorithm_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY backtest
@@ -474,7 +480,7 @@ ALTER TABLE ONLY backtest
 
 
 --
--- Name: backtest_ticker backtest_ticker_backtest_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: backtest_ticker backtest_ticker_backtest_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY backtest_ticker
@@ -482,7 +488,7 @@ ALTER TABLE ONLY backtest_ticker
 
 
 --
--- Name: backtest_ticker backtest_ticker_ticker_ticker_fk; Type: FK CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: backtest_ticker backtest_ticker_ticker_ticker_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY backtest_ticker
@@ -490,7 +496,7 @@ ALTER TABLE ONLY backtest_ticker
 
 
 --
--- Name: quote quote_ticker_ticker_fk; Type: FK CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: quote quote_ticker_ticker_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY quote
@@ -498,7 +504,7 @@ ALTER TABLE ONLY quote
 
 
 --
--- Name: quote quotes_day_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: quote quotes_day_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY quote
@@ -506,7 +512,7 @@ ALTER TABLE ONLY quote
 
 
 --
--- Name: result result_day_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: result result_day_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY result
@@ -514,7 +520,7 @@ ALTER TABLE ONLY result
 
 
 --
--- Name: result result_simulation_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: result result_simulation_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY result
@@ -522,7 +528,7 @@ ALTER TABLE ONLY result
 
 
 --
--- Name: simulation simulation_backtest_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: simulation simulation_backtest_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY simulation
@@ -530,7 +536,7 @@ ALTER TABLE ONLY simulation
 
 
 --
--- Name: simulation simulation_ticker_ticker_fk; Type: FK CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: simulation simulation_ticker_ticker_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY simulation
@@ -538,7 +544,7 @@ ALTER TABLE ONLY simulation
 
 
 --
--- Name: trade trade_result_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: backquote_user
+-- Name: trade trade_result_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY trade
