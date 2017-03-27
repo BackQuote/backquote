2be349b2085bea1ae7d91408c18612ca452c9a9e
Martin Rancourt <vonziper@gmail.com>
2017-03-26 22:39:08 -0400

    changed jsontree theming

--- client/components/SimulationRow.js
+++ client/components/SimulationRow.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import JSONTree from 'react-json-tree';
 import { Link } from 'react-router';
 import ProfitNumber from './ProfitNumber';
+import { theme } from '../themes/default';
 
 class SimulationRow extends React.Component {
   render() {
@@ -11,7 +12,7 @@ class SimulationRow extends React.Component {
           <Link to={`/simulation/${this.props.simulation.id}`}>{this.props.index}</Link>
         </td>
         <td>
-          <JSONTree data={this.props.simulation.params} hideRoot />
+          <JSONTree data={this.props.simulation.params} theme={theme} hideRoot />
         </td>
         <td>{this.props.simulation.ticker}</td>
         <td><ProfitNumber value={this.props.simulation.profitReset}/></td>
