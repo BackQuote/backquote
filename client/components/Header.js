b083caae8daeb590452bba0b79d2b32e66bc164c
Martin Rancourt <mrancourt@users.noreply.github.com>
2017-03-29 00:59:55 -0400

    added media query for the menu (#72)

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import { header } from '../styles/header.scss';
-import LoadingBar from 'react-redux-loading-bar';
 
-const Header = () =>
-    <div>
-      <header className={header}>
-        Backtester Platform
-      </header>
-      <LoadingBar style={{ backgroundColor: '#77b6ff', position: 'relative', top: 0, zIndex: 40}} />
-    </div>;
+const Header = () => <header className={header}> </header>;
 
 export default Header;
