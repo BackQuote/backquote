939ab6c50af4d44da3971b15d664e8c835d49da3
Martin Rancourt <vonziper@gmail.com>
2017-03-30 01:11:18 -0400

    added icons for execution status

--- client/components/Execution.js
+++ client/components/Execution.js
@@ -7,9 +7,12 @@ export default class Execution extends React.Component {
     let { execution } = this.props;
     return (
       <div>
-        { execution.pending ? 'Pending...' :
-          <Link to={`backtest/${execution.id}`}>Backtest #{execution.id}</Link>
-        }
+        <Link to={`backtest/${execution.id}`}>Backtest #{execution.id}</Link>{' '}
+        { execution.pending ? (
+          <small>(pending...)</small>
+        ) : execution.progress < 100  ? (
+          <i className="fa fa-refresh fa-spin"> </i>
+        ) : <i className="fa fa-check" style={{color: '#6CD899'}}> </i> }
         <Line percent={execution.progress} strokeColor="#33c3f0" trailColor="#f6f6f6" />
       </div>
     );
